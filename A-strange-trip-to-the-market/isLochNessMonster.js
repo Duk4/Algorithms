d01f63c05f05a4156495b110f511a772b476c024
Dule <crashendodt@gmail.com>
2019-11-03 19:47:12 +0100

    doing it right now

--- A-strange-trip-to-the-market/isLochNessMonster.js
+++ A-strange-trip-to-the-market/isLochNessMonster.js
@@ -2,5 +2,16 @@ function isLochNessMonster(s) {
     // Using ternary operator I checked if string s contained one of these strings
     // And returned true if it did, or false if it didn't
     // As Monster uses these phrases when speaking
-    return s.includes("tree fiddy" || "three fifty" || "3.50") ? true : false;
+    let y;
+
+    if (s.includes("tree fiddy")) {
+        y = true;
+    } else if (s.includes("3.50")) {
+        y = true;
+    } else if (s.includes("three fifty")) {
+        y = true;
+    } else {
+        y = false;
+    }
+    return y;
 }
