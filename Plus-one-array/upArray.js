1df79e48ee392dd33395f74968be9a78228a87d6
Dule <crashendodt@gmail.com>
2019-11-16 21:34:11 +0100

    update

--- Plus-one-array/upArray.js
+++ Plus-one-array/upArray.js
@@ -1,12 +1,25 @@
 function upArray(arr) {
-    arr = Number(arr.join(''));
-    if (isNaN(arr)) {
-        return null;
-    }
-    arr++;
-    arr = arr.toString().split('');
+    // returns null if array is empty or contains integers that are not 9 >= n >= 0
+    if (arr.length < 1 || arr == undefined) return null;
     for (let i = 0; i < arr.length; i++) {
-        arr[i] = Number(arr[i]);
+        if (arr[i] < 0 || arr[i] > 9) {
+            return null;
+        }
+    }
+    // going from back to front, switching 9's into 0's and incrementing next element by 1
+    for (let j = arr.length - 1; j >= 0; j--) {
+        if (arr[j] >= 9) {
+            arr[j] = 0;
+            if (j == 0) {           // if we have all 9's we convert them into 0's and put 1 in front of other elements
+                arr.unshift(1);
+            } else {
+                arr[j - 1]++;
+            }
+        } else {
+            arr[j]++;
+            break;
+        }
+        if (arr[j - 1] <= 9) break;
     }
     return arr;
 }
